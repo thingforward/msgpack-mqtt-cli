fdd6b522ee20edd4bb68e18604d673f39ad4e595
Andreas Schmidt <schm75a1koe@googlemail.com>
2017-11-17 12:35:06 +0100

    refactored

---
+++ src/json2msgpack.cpp
@@ -0,0 +1,205 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Bases on msgpack-tools/json2msgpack, copyright (c) 2015-2017 Nicholas Fraser
+ */
+
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include "mpack/mpack.h"
+#include "rapidjson/error/en.h"
+
+// libb64 doesn't have extern "C" around its C headers
+extern "C" {
+#include "b64/cdecode.h"
+#include "b64/cencode.h"
+}
+
+#define BUFFER_SIZE 65536
+
+#include <errno.h>
+#include "rapidjson/document.h"
+
+#include "mosquitto/mosquitto.h"
+
+using namespace rapidjson;
+
+typedef struct options_t {
+    const char* command;
+    const char* out_filename;
+    const char* in_filename;
+    bool lax;
+    bool use_float;
+    bool base64_prefix;
+    size_t base64_min_bytes;
+} options_t;
+
+static const char* prefix_ext    = "ext:";
+static const char* prefix_base64 = "base64:";
+
+// libb64 doesn't have the means to return errors on invalid characters, so
+// for base64 detection, we manually scan the string for invalid characters.
+// For detection we allow CR/LF but we don't allow spaces (otherwise pretty
+// much any normal string would be detected as base64.)
+static bool is_base64(const char* string, size_t length) {
+    for (size_t i = 0; i < length; ++i) {
+        char c = string[i];
+        if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')
+                || c == '+' || c == '/' || c == '=' || c == '\r' || c == '\n')) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static char* convert_base64(options_t* options, const char* p, size_t len, size_t* out_bytes) {
+    size_t bytes = len * 3 / 4 + 4; // TODO check this
+    char* data = (char*)malloc(bytes);
+    if (!data) {
+        fprintf(stderr, "%s: allocation failure\n", options->command);
+        return NULL;
+    }
+    base64_decodestate state;
+    base64_init_decodestate(&state);
+    bytes = base64_decode_block(p, len, data, &state);
+    *out_bytes = bytes;
+    return data;
+}
+
+static bool write_string(options_t* options, mpack_writer_t* writer, const char* string, size_t length, bool allow_detection) {
+
+    if (options->base64_prefix) {
+
+        // check for base64 prefix
+        if (length >= strlen(prefix_base64) && memcmp(string, prefix_base64, strlen(prefix_base64)) == 0) {
+            const char* base64_data = string + strlen(prefix_base64);
+            size_t base64_len = length - strlen(prefix_base64);
+            if (!is_base64(base64_data, base64_len)) {
+                fprintf(stderr, "%s: string prefixed with \"base64:\" contains invalid base64\n", options->command);
+                return false;
+            }
+
+            // write base64
+            size_t count;
+            char* bytes = convert_base64(options, base64_data, base64_len, &count);
+            if (bytes) {
+                mpack_write_bin(writer, bytes, count);
+                free(bytes);
+                return mpack_writer_error(writer) == mpack_ok;
+            }
+            return false;
+        }
+
+        // check for ext prefix
+        if (length >= strlen(prefix_ext) && memcmp(string, prefix_ext, strlen(prefix_ext)) == 0) {
+
+            // parse exttype
+            const char* exttype_str = string + strlen(prefix_ext);
+            char* remainder;
+            errno = 0;
+            int64_t exttype = strtol(exttype_str, &remainder, 10);
+            if (errno != 0 || *(remainder++) != ':' || strlen(remainder) < strlen(prefix_base64)
+                    || memcmp(remainder, prefix_base64, strlen(prefix_base64)) != 0) {
+                fprintf(stderr, "\"%s\"\n", remainder);
+                fprintf(stderr, "%s: string prefixed with \"ext:\" contains invalid prefix\n", options->command);
+                return false;
+            }
+            if (exttype < INT8_MIN || exttype > INT8_MAX) {
+                fprintf(stderr, "%s: string prefixed with \"ext:\" has out-of-bounds ext type: %" PRIi64 "\n", options->command, exttype);
+                return false;
+            }
+
+            // check base64
+            const char* base64_data = remainder + strlen(prefix_base64);
+            size_t base64_len = strlen(base64_data);
+            if (!is_base64(base64_data, base64_len)) {
+                fprintf(stderr, "\"%s\"\n", base64_data);
+                fprintf(stderr, "%s: string prefixed with \"ext:\" contains invalid base64\n", options->command);
+                return false;
+            }
+
+            // write ext
+            size_t count;
+            char* bytes = convert_base64(options, base64_data, base64_len, &count);
+            if (bytes) {
+                mpack_write_ext(writer, (int8_t)exttype, bytes, count);
+                free(bytes);
+                return mpack_writer_error(writer) == mpack_ok;
+            }
+            return false;
+        }
+
+    }
+
+    // try to parse as base64
+    if (allow_detection && options->base64_min_bytes != 0 && length >= options->base64_min_bytes && is_base64(string, length)) {
+        size_t count;
+        char* bytes = convert_base64(options, string, length, &count);
+        if (bytes) {
+            mpack_write_bin(writer, bytes, count);
+            free(bytes);
+            return mpack_writer_error(writer) == mpack_ok;
+        }
+        return false;
+    }
+
+    mpack_write_str(writer, string, length);
+    return mpack_writer_error(writer) == mpack_ok;
+}
+
+bool write_value(options_t* options, Value& value, mpack_writer_t* writer) {
+    switch (value.GetType()) {
+        case kNullType:   mpack_write_nil(writer);    break;
+        case kTrueType:   mpack_write_true(writer);   break;
+        case kFalseType:  mpack_write_false(writer);  break;
+
+        case kNumberType:
+            if (value.IsDouble()) {
+                if (options->use_float)
+                    mpack_write_float(writer, (float)value.GetDouble());
+                else
+                    mpack_write_double(writer, value.GetDouble());
+            } else if (value.IsUint64()) {
+                mpack_write_u64(writer, value.GetUint64());
+            } else {
+                mpack_write_i64(writer, value.GetInt64());
+            }
+            break;
+
+        case kStringType:
+            if (!write_string(options, writer, value.GetString(), value.GetStringLength(), true))
+                return false;
+            break;
+
+        case kArrayType: {
+            mpack_start_array(writer, value.Size());
+            Value::ValueIterator it = value.Begin(), end = value.End();
+            for (; it != end; ++it) {
+                if (!write_value(options, *it, writer))
+                    return false;
+            }
+            mpack_finish_array(writer);
+            break;
+        }
+
+        case kObjectType: {
+            mpack_start_map(writer, value.MemberCount());
+            Value::MemberIterator it = value.MemberBegin(), end = value.MemberEnd();
+            for (; it != end; ++it) {
+                if (!write_string(options, writer, it->name.GetString(), it->name.GetStringLength(), false))
+                    return false;
+                if (!write_value(options, it->value, writer))
+                    return false;
+            }
+            mpack_finish_map(writer);
+            break;
+        }
+
+        default:
+            return false;
+    }
+
+    return mpack_writer_error(writer) == mpack_ok;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#include "mpack/mpack.h"
-#include "rapidjson/error/en.h"
-
-// libb64 doesn't have extern "C" around its C headers
-extern "C" {
-#include "b64/cdecode.h"
-#include "b64/cencode.h"
-}
-
-#define BUFFER_SIZE 65536
 
 #include <errno.h>
+#include "rapidjson/rapidjson.h"
 #include "rapidjson/document.h"
+#include "rapidjson/error/en.h"
 #include "mpack/mpack.h"
-#include "rapidjson/document.h"
 
 #include "mosquitto/mosquitto.h"
 
@@ -36,175 +27,15 @@ typedef struct options_t {
     bool use_float;
     bool base64_prefix;
     size_t base64_min_bytes;
+
+    const char* mqtt_server_host;
+    int mqtt_server_port;
+    const char* mqtt_topic;
+    bool debug;
 } options_t;
 
-static const char* prefix_ext    = "ext:";
-static const char* prefix_base64 = "base64:";
-
-// libb64 doesn't have the means to return errors on invalid characters, so
-// for base64 detection, we manually scan the string for invalid characters.
-// For detection we allow CR/LF but we don't allow spaces (otherwise pretty
-// much any normal string would be detected as base64.)
-static bool is_base64(const char* string, size_t length) {
-    for (size_t i = 0; i < length; ++i) {
-        char c = string[i];
-        if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')
-                || c == '+' || c == '/' || c == '=' || c == '\r' || c == '\n')) {
-            return false;
-        }
-    }
-    return true;
-}
-
-static char* convert_base64(options_t* options, const char* p, size_t len, size_t* out_bytes) {
-    size_t bytes = len * 3 / 4 + 4; // TODO check this
-    char* data = (char*)malloc(bytes);
-    if (!data) {
-        fprintf(stderr, "%s: allocation failure\n", options->command);
-        return NULL;
-    }
-    base64_decodestate state;
-    base64_init_decodestate(&state);
-    bytes = base64_decode_block(p, len, data, &state);
-    *out_bytes = bytes;
-    return data;
-}
-
-static bool write_string(options_t* options, mpack_writer_t* writer, const char* string, size_t length, bool allow_detection) {
-
-    if (options->base64_prefix) {
-
-        // check for base64 prefix
-        if (length >= strlen(prefix_base64) && memcmp(string, prefix_base64, strlen(prefix_base64)) == 0) {
-            const char* base64_data = string + strlen(prefix_base64);
-            size_t base64_len = length - strlen(prefix_base64);
-            if (!is_base64(base64_data, base64_len)) {
-                fprintf(stderr, "%s: string prefixed with \"base64:\" contains invalid base64\n", options->command);
-                return false;
-            }
-
-            // write base64
-            size_t count;
-            char* bytes = convert_base64(options, base64_data, base64_len, &count);
-            if (bytes) {
-                mpack_write_bin(writer, bytes, count);
-                free(bytes);
-                return mpack_writer_error(writer) == mpack_ok;
-            }
-            return false;
-        }
-
-        // check for ext prefix
-        if (length >= strlen(prefix_ext) && memcmp(string, prefix_ext, strlen(prefix_ext)) == 0) {
-
-            // parse exttype
-            const char* exttype_str = string + strlen(prefix_ext);
-            char* remainder;
-            errno = 0;
-            int64_t exttype = strtol(exttype_str, &remainder, 10);
-            if (errno != 0 || *(remainder++) != ':' || strlen(remainder) < strlen(prefix_base64)
-                    || memcmp(remainder, prefix_base64, strlen(prefix_base64)) != 0) {
-                fprintf(stderr, "\"%s\"\n", remainder);
-                fprintf(stderr, "%s: string prefixed with \"ext:\" contains invalid prefix\n", options->command);
-                return false;
-            }
-            if (exttype < INT8_MIN || exttype > INT8_MAX) {
-                fprintf(stderr, "%s: string prefixed with \"ext:\" has out-of-bounds ext type: %" PRIi64 "\n", options->command, exttype);
-                return false;
-            }
-
-            // check base64
-            const char* base64_data = remainder + strlen(prefix_base64);
-            size_t base64_len = strlen(base64_data);
-            if (!is_base64(base64_data, base64_len)) {
-                fprintf(stderr, "\"%s\"\n", base64_data);
-                fprintf(stderr, "%s: string prefixed with \"ext:\" contains invalid base64\n", options->command);
-                return false;
-            }
-
-            // write ext
-            size_t count;
-            char* bytes = convert_base64(options, base64_data, base64_len, &count);
-            if (bytes) {
-                mpack_write_ext(writer, (int8_t)exttype, bytes, count);
-                free(bytes);
-                return mpack_writer_error(writer) == mpack_ok;
-            }
-            return false;
-        }
-
-    }
-
-    // try to parse as base64
-    if (allow_detection && options->base64_min_bytes != 0 && length >= options->base64_min_bytes && is_base64(string, length)) {
-        size_t count;
-        char* bytes = convert_base64(options, string, length, &count);
-        if (bytes) {
-            mpack_write_bin(writer, bytes, count);
-            free(bytes);
-            return mpack_writer_error(writer) == mpack_ok;
-        }
-        return false;
-    }
-
-    mpack_write_str(writer, string, length);
-    return mpack_writer_error(writer) == mpack_ok;
-}
-
-static bool write_value(options_t* options, Value& value, mpack_writer_t* writer) {
-    switch (value.GetType()) {
-        case kNullType:   mpack_write_nil(writer);    break;
-        case kTrueType:   mpack_write_true(writer);   break;
-        case kFalseType:  mpack_write_false(writer);  break;
-
-        case kNumberType:
-            if (value.IsDouble()) {
-                if (options->use_float)
-                    mpack_write_float(writer, (float)value.GetDouble());
-                else
-                    mpack_write_double(writer, value.GetDouble());
-            } else if (value.IsUint64()) {
-                mpack_write_u64(writer, value.GetUint64());
-            } else {
-                mpack_write_i64(writer, value.GetInt64());
-            }
-            break;
-
-        case kStringType:
-            if (!write_string(options, writer, value.GetString(), value.GetStringLength(), true))
-                return false;
-            break;
-
-        case kArrayType: {
-            mpack_start_array(writer, value.Size());
-            Value::ValueIterator it = value.Begin(), end = value.End();
-            for (; it != end; ++it) {
-                if (!write_value(options, *it, writer))
-                    return false;
-            }
-            mpack_finish_array(writer);
-            break;
-        }
-
-        case kObjectType: {
-            mpack_start_map(writer, value.MemberCount());
-            Value::MemberIterator it = value.MemberBegin(), end = value.MemberEnd();
-            for (; it != end; ++it) {
-                if (!write_string(options, writer, it->name.GetString(), it->name.GetStringLength(), false))
-                    return false;
-                if (!write_value(options, it->value, writer))
-                    return false;
-            }
-            mpack_finish_map(writer);
-            break;
-        }
-
-        default:
-            return false;
-    }
-
-    return mpack_writer_error(writer) == mpack_ok;
-}
+// from json2msgpack.cpp
+bool write_value(options_t* options, Value& value, mpack_writer_t* writer);
 
 static int transform(options_t *options, Document &document, char *p_buf, size_t sz_buf) {
     mpack_writer_t writer;
@@ -298,18 +129,9 @@ void connect_callback(struct mosquitto *mosq, void *obj, int result)
 
 void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message)
 {
-	bool match = 0;
-	printf("got message '%.*s' for topic '%s'\n", message->payloadlen, (char*) message->payload, message->topic);
-
-	mosquitto_topic_matches_sub("/devices/wb-adc/controls/+", message->topic, &match);
-	if (match) {
-		printf("got message for ADC topic\n");
-	}
-
 }
 
-
-int mqtt_publish(const char *p_topic, char *p_buf, size_t sz_buf)
+int mqtt_publish(options_t *options, char *p_buf, size_t sz_buf)
 {
 	uint8_t reconnect = true;
 	char clientid[24];
@@ -326,12 +148,27 @@ int mqtt_publish(const char *p_topic, char *p_buf, size_t sz_buf)
 		mosquitto_connect_callback_set(mosq, connect_callback);
 		mosquitto_message_callback_set(mosq, message_callback);
 
-	    	rc = mosquitto_connect(mosq, "127.0.0.1",1883, 5);
+    if ( options->debug) {
+      fprintf(stderr, "Connecting MQTT\n");
+    }
+  	rc = mosquitto_connect(mosq, options->mqtt_server_host, options->mqtt_server_port, 5);
+    if ( rc != 0) {
+      fprintf(stderr, "ERROR connecting to MQTT server at %s:%i\n", options->mqtt_server_host, options->mqtt_server_port);
+    }
 
-		mosquitto_publish(mosq, NULL, p_topic, sz_buf, p_buf, 1, false);
-		//mosquitto_destroy(mosq);
+    if ( options->debug) {
+      fprintf(stderr, "Connected, publishing to topic\n");
+    }
+		rc = mosquitto_publish(mosq, NULL, options->mqtt_topic, sz_buf, p_buf, 1, false);
+    if ( rc != 0) {
+      fprintf(stderr, "ERROR publishing to topic %s\n", options->mqtt_topic);
+    }
+    if ( options->debug) {
+      fprintf(stderr, "Done.\n");
+    }
+		mosquitto_destroy(mosq);
 	} else {
-		printf("Error creating client.\n");
+		fprintf(stderr, "Error creating mqtt client.\n");
 	}
 
 	mosquitto_lib_cleanup();
@@ -344,10 +181,19 @@ int mqtt_publish(const char *p_topic, char *p_buf, size_t sz_buf)
 static bool process(options_t *options) {
     char* data = NULL;
     size_t size = 0;
+
+    if ( options->debug) {
+      fprintf(stderr, "Reading input...\n");
+    }
+
     if (!load_file_or_stdin(options, &data, &size))
         return false;
 
     // The data has been null-terminated by load_file()
+    if ( options->debug) {
+      fprintf(stderr, "Converting JSON to Msgpack\n");
+    }
+
     Document document;
     if (options->lax)
         document.ParseInsitu<kParseFullPrecisionFlag | kParseCommentsFlag | kParseTrailingCommasFlag>(data);
@@ -364,8 +210,10 @@ static bool process(options_t *options) {
     char buf[1024];
     int len = transform(options, document, buf,sizeof(buf));
     if ( len >= 0) {
-      // pass on..
-      mqtt_publish("slyft", buf, len);
+      if ( options->debug) {
+        fprintf(stderr, "Publishing on topic %s\n", options->mqtt_topic);
+      }
+      mqtt_publish(options, buf, len);
     }
     free(data);
     return true;
@@ -388,36 +236,46 @@ static void parse_min_bytes(options_t* options) {
 }
 
 static void usage(const char* command) {
-    fprintf(stderr, "Usage: %s [-i <infile>] [-o <outfile>] [-lfb] [-B <min>]\n", command);
+    fprintf(stderr, "Usage: %s [-s <host|ip>] [-p <port>] [-t <topic>] [-i <infile>] [-o <outfile>] [-lfb] [-B <min>] [-d]\n", command);
     fprintf(stderr, "Options:\n");
+    fprintf(stderr, "    -s <host|ip>  Hostname or IP of MQTT server (default localhost)\n");
+    fprintf(stderr, "    -p <port>     Port of MQTT server (default 1883)\n");
+    fprintf(stderr, "    -t <topic>    Name of topic to publish on (default slyft)\n");
     fprintf(stderr, "    -i <infile>  Input filename (default stdin)\n");
     fprintf(stderr, "    -o <outfile>  Output filename (default stdout)\n");
     fprintf(stderr, "    -l  Lax mode, allows comments and trailing commas\n");
     fprintf(stderr, "    -f  Write floats instead of doubles\n");
     fprintf(stderr, "    -b  Convert base64 strings with \"base64:\" prefix to bin\n");
+    fprintf(stderr, "    -d  Turn on debug output\n");
     fprintf(stderr, "    -B <min>  Try to convert any base64 string of at least <min> bytes to bin\n");
     fprintf(stderr, "    -h  Print this help\n");
-    fprintf(stderr, "    -v  Print version information\n");
 }
 
-static void version(const char* command) {
-/*
-    fprintf(stderr, "%s version %s -- %s\n", command, VERSION, "https://github.com/ludocode/msgpack2json");
-    fprintf(stderr, "RapidJSON version %s -- %s\n", RAPIDJSON_VERSION_STRING, "http://rapidjson.org/");
-    fprintf(stderr, "MPack version %s -- %s\n", MPACK_VERSION_STRING, "https://github.com/ludocode/mpack");
-    fprintf(stderr, "libb64 version %s -- %s\n", LIBB64_VERSION, "http://libb64.sourceforge.net/");
-*/
-}
+#define OPTIONS_DEFAULT_MQTT_SERVER_HOST  "localhost"
+#define OPTIONS_DEFAULT_MQTT_SERVER_PORT 1883
+#define OPTIONS_DEFAULT_MQTT_TOPIC        "slyft"
 
 int main(int argc, char** argv) {
     options_t options;
     memset(&options, 0, sizeof(options));
     options.command = argv[0];
+    options.mqtt_server_host = OPTIONS_DEFAULT_MQTT_SERVER_HOST;
+    options.mqtt_server_port = OPTIONS_DEFAULT_MQTT_SERVER_PORT;
+    options.mqtt_topic = OPTIONS_DEFAULT_MQTT_TOPIC;
 
     opterr = 0;
     int opt;
-    while ((opt = getopt(argc, argv, "i:o:lfbB:hv?")) != -1) {
+    while ((opt = getopt(argc, argv, "h:p:i:o:lfbdB:hv?")) != -1) {
         switch (opt) {
+            case 's':
+                options.mqtt_server_host = optarg;
+                break;
+            case 'p':
+                options.mqtt_server_port = atoi(optarg);
+                break;
+            case 't':
+                options.mqtt_topic = optarg;
+                break;
             case 'i':
                 options.in_filename = optarg;
                 break;
@@ -433,14 +291,14 @@ int main(int argc, char** argv) {
             case 'b':
                 options.base64_prefix = true;
                 break;
+            case 'd':
+                options.debug = true;
+                break;
             case 'B':
                 parse_min_bytes(&options);
                 break;
             case 'h':
                 usage(options.command);
-                return EXIT_SUCCESS;
-            case 'v':
-                version(options.command);
                 return EXIT_SUCCESS;
             default: /* ? */
                 if (optopt == 0) {
